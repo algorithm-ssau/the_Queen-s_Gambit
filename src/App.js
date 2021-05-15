43c980332dab58246e0143bc5c739081c0a12e2b
gvazyukova <gvazyukova@mail.ru>
2021-05-16 00:00:50 +0400

    Добавлены страницы

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MainPage from "./components/main-page.component";
+import Episodes from "./components/Episodes/episodes.component";
+import Characters from "./components/characters.component";
+import Actors from "./components/actors.component";
+import Info from "./components/info.component";
+import "./App.css";
 
-function App() {
-  return (
-    <nav class="menu">
-        <div class="wrapCap"><label class="labCap">The Gambit Queen Fan</label></div>
-        
-        <ul>
-          <li class="cap pad"><a href="mainpage.html"><svg width="40" height="40" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M46.6666 24.5408L33.5416 11.4158L12.8741 32.0833H17.5V55.4167H26.25V37.9167H40.8333V55.4167H49.5833V32.0833H54.2091L49.5833 27.4575V17.5H46.6666V24.5408ZM5.83331 35L33.5416 7.29166L43.75 17.5V14.5833H52.5V26.25L61.25 35H52.5V58.3275H37.9166V40.8275H29.1666V58.3275H14.5833V35H5.83331Z" fill="white"/>
-        </svg></a></li>
-        <li class="cap"><a href="characters.html">Персонажи</a></li>
-        <li class="cap"><a href="#">Актеры</a></li>
-        <li class="cap"><a href="episodes.html">Серии</a></li>
-    </ul>
-      </nav>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route path="/" exact component={MainPage}/>
+          <Route path="/episodes" component={Episodes}/>
+          <Route path="/characters" component={Characters}/>
+          <Route path="/actors"  component={Actors}/>
+          <Route path="/info /:id" component={Info}/>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
+
